597de6107beea9fc0c625090373af8e0e2e02114
KS LIM <potatoscript@yahoo.com>
2021-09-25 22:32:07 +0900

    created project

---
+++ Models/Project.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication7.Models
+{
+    public class Project
+    {
+        public List<Project> project = new List<Project>();
+
+        public string Id { get; set; }
+        public string Text { get; set; }
+
+        public int ProjectId { get; set; }
+
+        public string title { get; set; }
+        public string owner { get; set; }
+
+        public string duedate { get; set; }
+
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+//window onload
+jQuery(document).ready(function() {
+
+    jQuery("#table_data td").click(function () {
+        var ri = jQuery(this).parent().parent().children().index(this.parentNode);
+        var table = document.getElementById("table_data");
+        document.getElementById("ProjectId").value = table.rows[ri].cells[0].innerText;
+        document.getElementById("title").value = table.rows[ri].cells[1].innerText;
+        document.getElementById("owner").value = table.rows[ri].cells[2].innerText;
+        document.getElementById("duedate").value = table.rows[ri].cells[3].innerText;
+
+    });
+
+
+});
